6bf13fce02e45345600eb9edb264d8e89a6875e9
Jordan Cullen <Jordan@shoemagoo.com>
2018-12-22 05:28:15 -0800

    more formatting

--- src/helpers/range.js
+++ src/helpers/range.js
@@ -3,16 +3,23 @@ const { LowerLetters, UpperLetters } = require("./constants");
 const Range = (first, last) => {
   // alphabetical range
   if (typeof first === "string" && typeof last === "string") {
-    // lower case set
     if (first === first.toLowerCase() && last == last.toLowerCase()) {
+      // lower case set
       let start = LowerLetters.indexOf(first);
       let stop = LowerLetters.indexOf(last);
+      if (stop < start) {
+        return [...Range(first, "z"), ...Range("a", last)];
+      }
 
       return LowerLetters.slice(start, stop + 1);
+      // uppercase
     } else if (first === first.toUpperCase() && last == last.toUpperCase()) {
       // upper case set
       let start = UpperLetters.indexOf(first);
       let stop = UpperLetters.indexOf(last);
+      if (stop < start) {
+        return [...Range(first, "Z"), ...Range("A", last)];
+      }
 
       return UpperLetters.slice(start, stop + 1);
     } else {
