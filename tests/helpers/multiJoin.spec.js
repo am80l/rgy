14ef92123dc097c13efc8d3fe0fa58723ce4a407
am80l <akaaustin@gmail.com>
2018-12-22 05:12:43 -0800

    Add tests for multiJoin

---
+++ tests/helpers/multiJoin.spec.js
@@ -0,0 +1,28 @@
+const multiJoin = require('../../src/helpers/multiJoin');
+
+describe('multiJoin', () => {
+  it(`Handles strings`, () => {
+    expect(multiJoin('foo')).toEqual('foo');
+    expect(multiJoin('foo bar')).toEqual('foo bar');
+  });
+
+  it(`Handles integers`, () => {
+    expect(multiJoin('1')).toEqual('1');
+  });
+
+  it(`Handles strings and integers with special characters`, () => {
+    expect(multiJoin('^')).toEqual('\\^');
+    expect(multiJoin('foo!')).toEqual('foo\\!');
+    expect(multiJoin('foo!bar*')).toEqual('foo\\!bar\\*');
+  });
+
+  it('Joins arrays', () => {
+    expect(multiJoin(['a', 'b', 'c'])).toEqual('a|b|c');
+  });
+
+  it('Errors on unsupported types', () => {
+    expect(() => {
+      multiJoin({});
+    }).toThrow();
+  });
+});
