0799fd98a2917d10dc5ec29f1a149ca7facef644
Michael Hartmayer <michaelhartmayer@gmail.com>
2018-12-22 16:43:52 -0800

    added documentation

--- src/helpers/multiJoin.js
+++ src/helpers/multiJoin.js
@@ -1,20 +1,29 @@
 const clean = require('./clean');
 
-const multiJoin = any => {
+/**
+ * Multi-Join
+ * 
+ * Flattens all arrays into one. Converts every item in the array to a string. 
+ * Cleans each item in the array. Joins each item in the array with a '|'.
+ * 
+ * @param {Array | String | Number} joinable Any item(s) to join.
+ * @returns {String} Joined joinable.
+ */
+const multiJoin = joinable => {
   // string or a number, just return it as a string
-  if (typeof any === 'string' || typeof any === 'number') return clean(any.toString());
+  if (typeof joinable === 'string' || typeof joinable === 'number') return clean(joinable.toString());
 
   // as an array
-  if (Array.isArray(any)) {
-    let multiJoined = any.reduce((joined, item) => {
+  if (Array.isArray(joinable)) {
+    let multiJoined = joinable.reduce((joined, item) => {
       return [...joined, multiJoin(item)];
     }, []);
 
     return multiJoined.join('|');
   }
 
-  // nothing else should be inside any
-  throw Error('{ any } only accepts strings, numbers, and arrays.');
+  // nothing else should be inside joinable
+  throw Error('{ joinable } only accepts strings, numbers, and arrays.');
 };
 
 module.exports = multiJoin;
