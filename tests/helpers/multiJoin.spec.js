8b2fe9aaaecc01ec96fed93e1d11067ae6c00b49
am80l <akaaustin@gmail.com>
2018-12-22 16:38:30 -0800

    Use jest recommended plugin to extend eslintrc

--- tests/helpers/multiJoin.spec.js
+++ tests/helpers/multiJoin.spec.js
@@ -1,26 +1,26 @@
-import multiJoin from "../../src/helpers/multiJoin";
+import multiJoin from '../../src/helpers/multiJoin';
 
-describe("multiJoin", () => {
+describe('multiJoin', () => {
   it(`Handles strings`, () => {
-    expect(multiJoin("foo")).toEqual("foo");
-    expect(multiJoin("foo bar")).toEqual("foo bar");
+    expect(multiJoin('foo')).toEqual('foo');
+    expect(multiJoin('foo bar')).toEqual('foo bar');
   });
 
   it(`Handles integers`, () => {
-    expect(multiJoin("1")).toEqual("1");
+    expect(multiJoin('1')).toEqual('1');
   });
 
   it(`Handles strings and integers with special characters`, () => {
-    expect(multiJoin("^")).toEqual("\\^");
-    expect(multiJoin("foo!")).toEqual("foo\\!");
-    expect(multiJoin("foo!bar*")).toEqual("foo\\!bar\\*");
+    expect(multiJoin('^')).toEqual('\\^');
+    expect(multiJoin('foo!')).toEqual('foo\\!');
+    expect(multiJoin('foo!bar*')).toEqual('foo\\!bar\\*');
   });
 
-  it("Joins arrays", () => {
-    expect(multiJoin(["a", "b", "c"])).toEqual("a|b|c");
+  it('Joins arrays', () => {
+    expect(multiJoin(['a', 'b', 'c'])).toEqual('a|b|c');
   });
 
-  it("Errors on unsupported types", () => {
+  it('Errors on unsupported types', () => {
     expect(() => {
       multiJoin({});
     }).toThrow();
